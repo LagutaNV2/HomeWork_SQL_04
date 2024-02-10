ddfbdfac236d895018260c6ca16d431238cd5f70
Natali LaGuta <n.v.laguta2023@gmail.com>
2024-02-10 11:49:29 +0200

    Change main.py

--- script_HomeWork/main.py
+++ script_HomeWork/main.py
@@ -1,13 +1,7 @@
 import configparser
 import json
-
-import config
-import psycopg2
-import q
 import sqlalchemy
-
 from sqlalchemy.orm import sessionmaker
-
 from models import create_tables, Publisher, Shop, Book, Stock, Sale
 
 
@@ -26,11 +20,15 @@ def add_tables_from_json(name_json):
         session.add(model(id=record.get('pk'), **record.get('fields')))
 
 
-def get_sales_pablishers(input_name):
-    selected = session.query(Book.title, Shop.name, Sale.price, Sale.date_sale).join(Publisher).join(Stock).join(
-        Shop).join(Sale).filter(Publisher.name.like(input_name))
+def get_sales_pablishers(param):
+    if param.isdigit():
+        selected = session.query(Book.title, Shop.name, Sale.price, Sale.count, Sale.date_sale).join(Publisher).join(Stock).join(
+        Shop).join(Sale).filter(Publisher.id == int(param))
+    else:
+        selected = session.query(Book.title, Shop.name, Sale.price, Sale.count, Sale.date_sale).join(Publisher).join(Stock).join(
+            Shop).join(Sale).filter(Publisher.name.like(param))
     for s in selected.all():
-        print(f'{s[0]:<50} | {s[1]:<15} | {str(s[2])} | {str(s[3])}')
+        print(f'{s[0]:<50} | {s[1]:<15} | {str(s[2]*s[3]):<10} | {str(s[4])}')
 
 if __name__ == '__main__':
     config = configparser.ConfigParser()
@@ -44,7 +42,7 @@ if __name__ == '__main__':
     add_tables_from_json('tests_data.json')
     session.commit()
 
-    input_name = input('Введите название издателя: ')
-    get_sales_pablishers(input_name)
+    param = input('Введите код или название издателя: ')
+    get_sales_pablishers(param)
 
     session.close()
